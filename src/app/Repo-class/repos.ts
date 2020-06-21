5eea667f10b3f5aaf2ab82610718e282d86f93c0
Linda Tonui <tonuilinda@gmail.com>
2020-06-22 00:57:41 +0300

    add class repo

--- src/app/Repo-class/repos.ts
+++ src/app/Repo-class/repos.ts
@@ -1,2 +1,10 @@
 export class Repos {
+    constructor(
+        public email:any,
+        public public_repos:number,
+        public public_gists:number,
+        public bio:string,
+        public html_url:any,
+        public created_at:any
+        ){}
 }
