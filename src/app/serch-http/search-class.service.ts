234eaa1e1d31344281301e24170a0511aacd1182
Linda Tonui <tonuilinda@gmail.com>
2020-06-21 17:13:35 +0300

    add the api url to environment.ts

--- src/app/serch-http/search-class.service.ts
+++ src/app/serch-http/search-class.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { User } from '../user-class/user';
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class SearchClassService {
 
-  constructor() { }
+  user:User;
+
+  constructor(private http:HttpClient) { 
+    this.user= new User('','');
+  }
+userNameRequest(){
+  interface ApiResponse{
+    userName:string;
+    name:string;
+  }
+  let promise = new Promise((resolve,reject)=>{
+    this.http.get<ApiResponse>(environment.apiUrl).toPromise().then(response=>{
+      this.user.userName = response.userName
+      this.user.name = response.name
+
+      resolve()
+    },
+    error=>{
+     this.user.userName="MemoTonui";
+     this.user.name="MemoTonui";
+
+      reject(error)
+    })
+  })
+  return promise
 }
+}
+
+
